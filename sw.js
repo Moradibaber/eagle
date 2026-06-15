fe474336d449a1bbcc805f53b92fb8681ee60a3c
Moradibaber <mmoradibabersad@gmail.com>
2026-06-15 07:52:02 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
 
+const CACHE_NAME = 'attendance-cache-v1';
+
+const urlsToCache = [
+
+  './',
+  './index.html',
+  './admin.html'
+
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+    .then(cache => {
+
+      return cache.addAll(urlsToCache);
+
+    })
+
+  );
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+    .then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+});
+
+
