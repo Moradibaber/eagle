d5e791788303564fa18a784059bb5b20102e541e
Moradibaber <mmoradibabersad@gmail.com>
2026-06-19 15:54:57 +0330

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'attendance-pwa-v2';
+
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(APP_SHELL);
+            })
+            .then(() => {
+                return self.skipWaiting();
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(cacheNames => {
+                return Promise.all(
+                    cacheNames
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
+                );
+            })
+            .then(() => {
+                return self.clients.claim();
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(networkResponse => {
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('./index.html');
+                        }
+                    });
+            })
+    );
+});
