be39729486f79b809572ef9a85017472d83ff06e
Moradibaber <mmoradibabersad@gmail.com>
2026-06-21 12:27:16 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'attendance-pwa-v7';
+const CACHE_NAME = 'attendance-pwa-v8';
 
 const APP_SHELL = [
     './',
@@ -8,25 +8,54 @@ const APP_SHELL = [
     './icon-192.png',
     './icon-512.png'
 ];
+
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
     );
+
     self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys =>
-            Promise.all(keys.map(key => caches.delete(key)))
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
         )
     );
+
     self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
-});
 
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request)
+                    .catch(() => {
+
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('./index.html');
+                        }
+
+                    });
+
+            })
+    );
+
+});
