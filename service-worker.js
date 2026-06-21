e347812439bc49256cea106ec6f9f92ab54dc025
Moradibaber <mmoradibabersad@gmail.com>
2026-06-21 10:37:32 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,56 +8,25 @@ const APP_SHELL = [
     './icon-192.png',
     './icon-512.png'
 ];
-
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(APP_SHELL);
-            })
-            .then(() => {
-                return self.skipWaiting();
-            })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
     );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys()
-            .then(cacheNames => {
-                return Promise.all(
-                    cacheNames
-                        .filter(cacheName => cacheName !== CACHE_NAME)
-                        .map(cacheName => caches.delete(cacheName))
-                );
-            })
-            .then(() => {
-                return self.clients.claim();
-            })
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => caches.delete(key)))
+        )
     );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') {
-        return;
-    }
-
     event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request)
-                    .then(networkResponse => {
-                        return networkResponse;
-                    })
-                    .catch(() => {
-                        if (event.request.mode === 'navigate') {
-                            return caches.match('./index.html');
-                        }
-                    });
-            })
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
+
